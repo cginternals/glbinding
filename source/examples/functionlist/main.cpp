1290c56c2ed0c1c2d44176ea9df015f0bf5387ff
Daniel Limberger <daniel.limberger@hpi.uni-potsdam.de>
2014-05-12 14:19:42 +0200

    remove glfw from glbinding and resolve proc addresses platform specific

--- source/examples/functionlist/main.cpp
+++ source/examples/functionlist/main.cpp
@@ -6,12 +6,21 @@
 
 #include <glbinding/AbstractFunction.h>
 
+#include <iostream>
+
+void errorfun(int errnum, const char* errmsg)
+{
+    std::cerr << errnum << ": " << errmsg << std::endl;
+}
+
 int main(int /*argc*/, char* /*argv*/[])
 {
     if (!glfwInit())
     {
         return 1;
     }
+
+    glfwSetErrorCallback(errorfun);
 
     GLFWwindow* window = glfwCreateWindow(640, 480, "Dummy Window", NULL, NULL);
     if (!window)
--- source/glbinding/source/GetProcAddress.cpp
+++ source/glbinding/source/GetProcAddress.cpp
@@ -1,12 +1,52 @@
 #include <glbinding/GetProcAddress.h>
 
-#include <GLFW/glfw3.h>
+#ifdef WIN32
+    #include <string>
+    #include <windows.h>
+#elif __APPLE__
+    #include <string>
+    #include <mach-o/dyld.h>
+#else
+    #include <GL/glx.h>
+#endif
 
 namespace gl {
 
 ProcAddress GetProcAddress(const char * name)
 {
-    return glfwGetProcAddress(name);
+#ifdef WIN32
+
+    typedef void (__stdcall * PROCADDRESS)();
+    PROCADDRESS procAddress = reinterpret_cast<PROCADDRESS>(wglGetProcAddress(name));
+
+    if(nullptr == procAddress)
+    {
+        static HMODULE module = LoadLibrary(L"OPENGL32.DLL");
+        procAddress = reinterpret_cast<PROCADDRESS>(::GetProcAddress(module, name));
+    }
+
+#elif __APPLE__
+
+    typedef void * PROCADDRESS;
+
+    std::string string(name);
+    string = "_" + string;
+
+    NSSymbol symbol(nullptr);
+
+    if (NSIsSymbolNameDefined(string.c_str()))
+        symbol = NSLookupAndBindSymbol(string.c_str());
+
+    PROCADDRESS procAddress = reinterpret_cast<PROCADDRESS>(symbol ? NSAddressOfSymbol(symbol) : nullptr);
+
+#else
+
+    typedef void (const unsigned char * PROCADDRESS)();
+    PROCADDRESS procAddress = reinterpret_cast<PROCADDRESS>(glxGetProcAddress(name));
+
+#endif
+
+    return reinterpret_cast<ProcAddress>(procAddress);
 }
 
 } // namespace gl
