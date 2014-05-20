c32e5af3fde1e2503ecbc02a0ee11338698def21
Roland Lux <rollux2000@gmail.com>
2014-05-20 16:12:23 +0200

    Add glew comparison example with time measurements

---
+++ source/examples/comparison/main.cpp
@@ -0,0 +1,241 @@
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+
+#include <glbinding/glbinding.h>
+#include <glbinding/AbstractFunction.h>
+#include <glbinding/functions.h>
+#include <glbinding/constants.h>
+#include <glbinding/meta.h>
+#include <glbinding/GLenum.h>
+
+#include <iostream>
+#include <chrono>
+#include <ratio>
+
+class Timer
+{
+public:
+    Timer() : m_msg(nullptr) {}
+
+    void start(const char * msg)
+    {
+        m_msg = msg;
+        time = std::chrono::system_clock::now();
+    }
+
+    void stop()
+    {
+        auto delta = std::chrono::system_clock::now() - time;
+
+        float ms = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(delta).count();
+
+        std::cout << m_msg << ": " << ms << " ms" << std::endl;
+    }
+
+    void restart(const char * msg)
+    {
+        stop();
+        start(msg);
+    }
+
+protected:
+    const char * m_msg;
+    std::chrono::time_point<std::chrono::system_clock> time;
+};
+
+void errorfun(int errnum, const char * errmsg)
+{
+    std::cerr << errnum << ": " << errmsg << std::endl;
+}
+
+const char* vertexShaderCode = R"(
+#version 140
+#extension GL_ARB_explicit_attrib_location : require
+
+layout (location = 0) in vec2 corner;
+
+out vec4 color;
+
+void main()
+{
+gl_Position = vec4(corner * 2.0 - 1.0, 0.0, 1.0);
+color = vec4(corner, 0.0, 1.0);
+}
+
+)";
+const char* fragmentShaderCode = R"(
+#version 140
+#extension GL_ARB_explicit_attrib_location : require
+
+layout (location = 0) out vec4 fragColor;
+
+in vec4 color;
+
+void main()
+{
+fragColor = color;
+}
+
+)";
+
+struct vec2 { float x; float y; };
+
+const vec2 cornerData[4] = {
+    vec2{0, 0},
+    vec2{1, 0},
+    vec2{0, 1},
+    vec2{1, 1}
+};
+
+//------------------------------------------------------------------------
+
+void glewExample()
+{
+    GLuint vaoId;
+    GLuint cornerBufferId;
+    glGenVertexArrays(1, &vaoId);
+    glGenBuffers(1, &cornerBufferId);
+    GLuint programId = glCreateProgram();
+
+    GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShaderId, 1, &vertexShaderCode, 0);
+    glCompileShader(vertexShaderId);
+
+    GLuint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderId, 1, &fragmentShaderCode, 0);
+    glCompileShader(fragmentShaderId);
+
+    glAttachShader(programId, vertexShaderId);
+    glAttachShader(programId, fragmentShaderId);
+    glLinkProgram(programId);
+
+    glBindBuffer(GL_ARRAY_BUFFER, cornerBufferId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vec2)*4, cornerData, GL_STATIC_DRAW);
+
+    glBindVertexArray(vaoId);
+
+    GLint attributeIndex = glGetAttribLocation(programId, "corner");
+    GLint bindingIndex = 0;
+    glVertexAttribBinding(attributeIndex, bindingIndex);
+    glBindVertexBuffer(bindingIndex, cornerBufferId, 0, sizeof(vec2));
+    glVertexAttribFormat(attributeIndex, 2, GL_FLOAT, GL_FALSE, 0);
+    glEnableVertexAttribArray(attributeIndex);
+
+    glUseProgram(programId);
+
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+    glDeleteProgram(programId);
+    glDeleteBuffers(1, &cornerBufferId);
+    glDeleteVertexArrays(1, &vaoId);
+}
+
+void glbindingExample()
+{
+    gl::GLuint vaoId;
+    gl::GLuint cornerBufferId;
+    gl::GenVertexArrays(1, &vaoId);
+    gl::GenBuffers(1, &cornerBufferId);
+    gl::GLuint programId = gl::CreateProgram();
+
+    gl::GLuint vertexShaderId = gl::CreateShader(gl::VERTEX_SHADER);
+    gl::ShaderSource(vertexShaderId, 1, &vertexShaderCode, 0);
+    gl::CompileShader(vertexShaderId);
+
+    gl::GLuint fragmentShaderId = gl::CreateShader(gl::FRAGMENT_SHADER);
+    gl::ShaderSource(fragmentShaderId, 1, &fragmentShaderCode, 0);
+    gl::CompileShader(fragmentShaderId);
+
+    gl::AttachShader(programId, vertexShaderId);
+    gl::AttachShader(programId, fragmentShaderId);
+    gl::LinkProgram(programId);
+
+    gl::BindBuffer(gl::ARRAY_BUFFER, cornerBufferId);
+    gl::BufferData(gl::ARRAY_BUFFER, sizeof(vec2)*4, cornerData, gl::STATIC_DRAW);
+
+    gl::BindVertexArray(vaoId);
+
+    gl::GLint attributeIndex = gl::GetAttribLocation(programId, "corner");
+    gl::GLint bindingIndex = 0;
+    gl::VertexAttribBinding(attributeIndex, bindingIndex);
+    gl::BindVertexBuffer(bindingIndex, cornerBufferId, 0, sizeof(vec2));
+    gl::VertexAttribFormat(attributeIndex, 2, gl::FLOAT, gl::FALSE_, 0);
+    gl::EnableVertexAttribArray(attributeIndex);
+
+    gl::UseProgram(programId);
+
+    gl::DrawArrays(gl::TRIANGLE_STRIP, 0, 4);
+
+    gl::DeleteProgram(programId);
+    gl::DeleteBuffers(1, &cornerBufferId);
+    gl::DeleteVertexArrays(1, &vaoId);
+}
+
+void compare()
+{
+    Timer timer;
+
+    timer.start("glewInit()");
+
+    if (glewInit() != GLEW_OK)
+        return;
+
+    timer.restart("gl::initialize()");
+
+    if (!gl::initialize())
+        return;
+
+    timer.restart("glew calls");
+
+    glewExample();
+
+    timer.restart("glbinding calls");
+
+    glbindingExample();
+
+    timer.stop();
+
+    gl::AbstractFunction::setCallbackLevelForAllExcept(gl::AbstractFunction::CallbackLevel::All, { "glGetError" });
+    gl::AbstractFunction::setBeforeCallback([](const gl::AbstractFunction & ) {});
+    gl::AbstractFunction::setAfterCallback([](const gl::AbstractFunction &) {
+        gl::GLenum error = gl::GetError();
+        if (error.value != gl::NO_ERROR_)
+        {
+            std::cout << "Error: " << error.toString() << std::endl;
+        }
+    });
+    gl::AbstractFunction::setParametersCallback([](const gl::AbstractFunction &, const std::vector<gl::AbstractValue*> & ) {});
+    gl::AbstractFunction::setReturnValueCallback([](const gl::AbstractFunction &, const gl::AbstractValue* ) {});
+
+    timer.start("glbinding calls with callbacks");
+
+    glbindingExample();
+
+    timer.stop();
+}
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+    if (!glfwInit())
+    {
+        return 1;
+    }
+
+    glfwSetErrorCallback(errorfun);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Dummy Window", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    compare();
+
+    glfwTerminate();
+    return 0;
+}
