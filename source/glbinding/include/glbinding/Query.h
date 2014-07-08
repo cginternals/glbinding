a2f43d05561cff6a175a8fa9a80bef8eb91d59b3
Daniel Limberger <daniel.limberger@hpi.de>
2014-07-08 23:21:41 +0200

    add basic queries (subject for discussion)

---
+++ source/glbinding/include/glbinding/Query.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <glbinding/glbinding_api.h>
+
+#include <set>
+#include <string>
+
+
+namespace gl
+{
+
+enum class GLextension;
+class Version;
+
+
+class GLBINDING_API Query
+{
+public:
+    Query() = delete;
+
+    static std::set<GLextension> extensions(std::set<std::string> * unknown = nullptr);
+
+    static std::string renderer();
+    static std::string vendor();
+
+    static Version version();
+};
+
+} // namespace gl
