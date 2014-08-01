c4ac5707a8eefc5f0050c25fa76779b8df5a6f2b
Willy Scheibel <willyscheibel@gmx.de>
2014-08-01 14:12:04 +0200

    Update update script and gl.xml

--- codegeneration/update.sh
+++ codegeneration/update.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-wget -N https://cvs.khronos.org/svn/repos/ogl/trunk/doc/registry/public/api/gl.xml
+url=https://cvs.khronos.org/svn/repos/ogl/trunk/doc/registry/public/api/gl.xml
+
+if [ -e gl.revision ]
+then
+  rcurrent=`cat gl.revision`
+else
+  rcurrent=0
+fi
+
+echo "local revision of gl.xml is $rcurrent"
+echo "retrieving latest changed revision of gl.xml"
+
+rnew=`svn info "$url" | grep "Revision: " | awk '{ print $2}'`
+
+if [ $rnew -gt $rcurrent ]
+then
+  echo "updating gl.xml $rcurrent to $rnew"
+  svn export --force --quiet "$url"
+  echo "$rnew" > gl.revision
+  echo "local revision of gl.xml is $rnew"
+  echo ""
+fi
--- source/glbinding/source/glrevision.h
+++ source/glbinding/source/glrevision.h
@@ -2,5 +2,5 @@
 
 namespace 
 {
-	const unsigned int GL_REVISION = 27498;
+	const unsigned int GL_REVISION = 27590;
 }
