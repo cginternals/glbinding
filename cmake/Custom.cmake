8bd00623e8e0e7b5b1081f7ee0f1ca2b9d6b299a
Daniel Limberger <daniel.limberger@hpi.uni-potsdam.de>
2014-09-18 19:13:10 +0200

    refine install of examples * provide glew binaries on win32 if comparison example * provide qt binaries, plugins, and icu binaries on win32 if qt-cubescape example

--- cmake/Custom.cmake
+++ cmake/Custom.cmake
@@ -46,3 +46,48 @@ macro(list_extract OUTPUT REGEX)
     endforeach()
 
 endmacro()
+
+
+
+macro(install_qt COMP DEST)
+
+    set(args ${ARGV})
+    list(REMOVE_AT args 0)
+    list(REMOVE_AT args 0)
+
+    foreach(target ${args})
+        get_target_property(qtrelease Qt5::${target} LOCATION_RELEASE)
+        install(FILES ${qtrelease} DESTINATION ${DEST} COMPONENT ${COMP})
+    endforeach() 
+
+    get_target_property(qtrelease Qt5::Core LOCATION_RELEASE)
+    get_filename_component(qtdir ${qtrelease} DIRECTORY)
+
+    install(FILES 
+        ${qtdir}/icudt52.dll
+        ${qtdir}/icuin52.dll
+        ${qtdir}/icuuc52.dll
+        DESTINATION ${DEST}
+        COMPONENT ${COMP})
+
+endmacro()
+
+
+
+macro(install_qt_platforms COMP DEST)
+
+    set(args ${ARGV})
+    list(REMOVE_AT args 0)
+    list(REMOVE_AT args 0)
+
+    get_target_property(qtrelease Qt5::Core LOCATION_RELEASE)
+    get_filename_component(qtdir ${qtrelease} DIRECTORY)
+
+    foreach(target ${args})
+        install(FILES "${qtdir}/../plugins/platforms/${target}.dll" DESTINATION ${DEST}/plugins/platforms COMPONENT ${COMP})
+    endforeach()    
+
+    install(FILES ${CMAKE_SOURCE_DIR}/packages/qt.conf DESTINATION ${DEST} COMPONENT ${COMP})
+
+endmacro()
+
