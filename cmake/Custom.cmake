f07a3294e9a0a4c4cf90bd10027dd5c2c672e7f6
unknown <Daniel.Limberger@fb03h222ws03.hpi.uni-potsdam.de>
2014-11-25 13:49:20 +0100

    fix provisioning of qt dependencies in debug install

--- cmake/Custom.cmake
+++ cmake/Custom.cmake
@@ -51,13 +51,17 @@ endmacro()
 
 macro(install_qt COMP DEST)
 
+    if(WIN32)
+
     set(args ${ARGV})
     list(REMOVE_AT args 0)
     list(REMOVE_AT args 0)
 
     foreach(target ${args})
         get_target_property(qtrelease Qt5::${target} LOCATION_RELEASE)
-        install(FILES ${qtrelease} DESTINATION ${DEST} COMPONENT ${COMP})
+        install(FILES ${qtrelease} DESTINATION ${DEST} CONFIGURATIONS Release COMPONENT ${COMP})
+        get_target_property(qtdebug Qt5::${target} LOCATION_DEBUG)
+        install(FILES ${qtdebug} DESTINATION ${DEST}  CONFIGURATIONS Debug COMPONENT ${COMP})
     endforeach() 
 
     get_target_property(qtrelease Qt5::Core LOCATION_RELEASE)
@@ -70,23 +74,36 @@ macro(install_qt COMP DEST)
         DESTINATION ${DEST}
         COMPONENT ${COMP})
 
+    endif()
+
 endmacro()
 
 
 
 macro(install_qt_platforms COMP DEST)
 
+    if(WIN32)
+
     set(args ${ARGV})
     list(REMOVE_AT args 0)
     list(REMOVE_AT args 0)
 
     get_target_property(qtrelease Qt5::Core LOCATION_RELEASE)
-    get_filename_component(qtdir ${qtrelease} DIRECTORY)
+    get_filename_component(qtrelease_dir ${qtrelease} DIRECTORY)
+
+    # eventhoug it is the same directory, this script should not rely on it
+    get_target_property(qtdebug Qt5::Core LOCATION_DEBUG)
+    get_filename_component(qtdebug_dir ${qtdebug} DIRECTORY)
 
     foreach(target ${args})
-        install(FILES "${qtdir}/../plugins/platforms/${target}.dll" DESTINATION ${DEST}/plugins/platforms COMPONENT ${COMP})
+        install(FILES "${qtrelease_dir}/../plugins/platforms/${target}.dll" DESTINATION ${DEST}/plugins/platforms 
+            CONFIGURATIONS Release COMPONENT ${COMP})
+        install(FILES "${qtdebug_dir}/../plugins/platforms/${target}d.dll" DESTINATION ${DEST}/plugins/platforms 
+            CONFIGURATIONS Debug COMPONENT ${COMP})
     endforeach()    
 
     install(FILES ${CMAKE_SOURCE_DIR}/packages/qt.conf DESTINATION ${DEST} COMPONENT ${COMP})
+
+    endif()
 
 endmacro()
