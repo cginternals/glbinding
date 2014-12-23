5726154e1666dedc91e686f206f0a4ff7d0e2b6a
cgcostume <daniel.limberger@hpi.de>
2014-12-23 11:30:34 +0100

    adjustments to cmake
    * disable msvc14 warning for qt includes
    * use latest cmake for using qt

--- cmake/PlatformWindowsMSVC.cmake
+++ cmake/PlatformWindowsMSVC.cmake
@@ -38,6 +38,7 @@ set(WIN32_COMPILE_FLAGS
     # /wd4127       # -> conditional expression is constant
       /wd4251       # -> 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
       /wd4267       # -> 'var' : conversion from 'size_t' to 'type', possible loss of data
+      
 
     # /W3           # -> warning level 3
       /W4           # -> warning level 4
