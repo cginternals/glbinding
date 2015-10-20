90c6510d7a6c6bd591f139be0819053e797a55f7
Daniel Limberger <daniel.limberger@hpi.de>
2015-10-20 19:19:09 +0200

    remove unused headers

--- source/glbinding/source/Binding_objects_a.cpp
+++ source/glbinding/source/Binding_objects_a.cpp
@@ -1,10 +1,5 @@
 #include <glbinding/Binding.h>
 
-#include <glbinding/gl/bitfield.h>
-#include <glbinding/gl/boolean.h>
-#include <glbinding/gl/enum.h>
-#include <glbinding/gl/values.h>
-
 
 using namespace gl; // ToDo: multiple APIs?
 
