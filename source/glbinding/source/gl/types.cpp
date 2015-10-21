36f4b5d3e1890cd402c48541031c8c21723fcb64
Daniel Limberger <daniel.limberger@hpi.de>
2015-10-21 13:58:22 +0200

    best of both worlds - probably sticking with precompiled header (for splitted sources only, not for the project, not public)

--- source/glbinding/source/gl/types.cpp
+++ source/glbinding/source/gl/types.cpp
@@ -46,6 +46,22 @@ std::string bitfieldString(T value)
 
 
 
+std::ostream & operator<<(std::ostream & stream, const gl::GLextension & value)
+{
+    stream << glbinding::Meta::getString(value);
+    return stream;
+}
+
+
+
+std::ostream & operator<<(std::ostream & stream, const gl::GLboolean & value)
+{
+    stream << glbinding::Meta::getString(value);
+    return stream;
+}
+
+
+
 std::ostream & operator<<(std::ostream & stream, const gl::GLenum & value)
 {
     stream << glbinding::Meta::getString(value);
@@ -122,22 +138,6 @@ bool operator>(std::underlying_type<gl::GLenum>::type a, const gl::GLenum & b)
 bool operator>=(std::underlying_type<gl::GLenum>::type a, const gl::GLenum & b)
 {
     return a >= static_cast<std::underlying_type<gl::GLenum>::type>(b);
-}
-
-
-
-std::ostream & operator<<(std::ostream & stream, const gl::GLextension & value)
-{
-    stream << glbinding::Meta::getString(value);
-    return stream;
-}
-
-
-
-std::ostream & operator<<(std::ostream & stream, const gl::GLboolean & value)
-{
-    stream << glbinding::Meta::getString(value);
-    return stream;
 }
 
 
