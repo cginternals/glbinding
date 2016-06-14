f48446126c9174b183085bd71ca0abe9af47002f
Willy Scheibel <willy.scheibel@hpi.de>
2016-06-14 10:01:04 +0200

    Use static GLboolean members instead of new instances of GL_TRUE and GL_FALSE

--- source/glbinding/include/glbinding/gl/boolean.h
+++ source/glbinding/include/glbinding/gl/boolean.h
@@ -41,8 +41,8 @@ protected:
 
 // import booleans to namespace
 
-static const GLboolean GL_FALSE = GLboolean(false);
-static const GLboolean GL_TRUE = GLboolean(true);
+static const GLboolean GL_FALSE = GLboolean::GL_FALSE;
+static const GLboolean GL_TRUE = GLboolean::GL_TRUE;
 
 
 } // namespace gl
