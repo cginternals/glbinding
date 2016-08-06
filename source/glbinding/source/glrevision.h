b4f656ce3c89e1d794d84c067895cfa30a4b9e3d
Willy Scheibel <willy.scheibel@hpi.de>
2016-08-06 12:19:19 +0200

    Update gl.xml

--- source/glbinding/source/glrevision.h
+++ source/glbinding/source/glrevision.h
@@ -4,7 +4,7 @@ namespace glbinding
 {
 
 
-const unsigned int GL_REVISION = 33061; ///< The revision of the gl.xml at the time of code generation.
+const unsigned int GL_REVISION = 33080; ///< The revision of the gl.xml at the time of code generation.
 
 
 } // namespace glbinding
