1bc4a048234283d14e9c27f4c7fa53b1fea0e22b
Willy Scheibel <willy.scheibel@hpi.de>
2016-11-09 09:28:57 +0100

    Update gl.xml (no new symbols for OpenGL)

--- source/glbinding/source/glrevision.h
+++ source/glbinding/source/glrevision.h
@@ -4,7 +4,7 @@ namespace glbinding
 {
 
 
-const unsigned int GL_REVISION = 33224; ///< The revision of the gl.xml at the time of code generation.
+const unsigned int GL_REVISION = 33260; ///< The revision of the gl.xml at the time of code generation.
 
 
 } // namespace glbinding
