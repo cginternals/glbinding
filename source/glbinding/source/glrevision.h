21284ff1e0ab4d5b9cb8e1d3d4acc2f874a89d98
Willy Scheibel <willy.scheibel@hpi.de>
2016-11-30 10:03:33 +0100

    Update gl.xml (although no change to the OpenGL API)

--- source/glbinding/source/glrevision.h
+++ source/glbinding/source/glrevision.h
@@ -4,7 +4,7 @@ namespace glbinding
 {
 
 
-const unsigned int GL_REVISION = 33287; ///< The revision of the gl.xml at the time of code generation.
+const unsigned int GL_REVISION = 33312; ///< The revision of the gl.xml at the time of code generation.
 
 
 } // namespace glbinding
