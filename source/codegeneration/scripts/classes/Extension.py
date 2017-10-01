ca31ffae3630774d494b2507466e73de6415ffb3
Willy Scheibel <willy.scheibel@cginternals.com>
2017-10-01 09:28:01 +0200

    Add OpenGL 4.6 required extension information (closes #242)

--- source/codegeneration/scripts/classes/Extension.py
+++ source/codegeneration/scripts/classes/Extension.py
@@ -263,6 +263,19 @@ def resolveExtensions(extensions, enumsByName, commandsByName, features, api):
 			"GL_KHR_robustness",
 			"GL_ARB_shader_texture_image_samples",
 			"GL_ARB_texture_barrier"
+			],
+		"4.6": [
+			"GL_ARB_indirect_parameters",
+			"GL_ARB_pipeline_statistics_query",
+			"GL_ARB_polygon_offset_clamp",
+			"GL_KHR_no_error",
+			"GL_ARB_shader_atomic_counter_ops",
+			"GL_ARB_shader_draw_parameters",
+			"GL_ARB_shader_group_vote",
+			"GL_ARB_gl_spirv",
+			"GL_ARB_spirv_extensions",
+			"GL_ARB_texture_filter_anisotropic",
+			"GL_ARB_transform_feedback_overflow_query"
 			]
 		}
 
