b5fc8ad2fb4baaab635181c8c4a6ac5f7d4e1b16
Willy Scheibel <willy.scheibel@hpi.de>
2018-03-08 20:57:11 +0100

    Prepare loading of other xml interface specification files

--- source/codegeneration/scripts/update.py
+++ source/codegeneration/scripts/update.py
@@ -3,9 +3,9 @@
 import os, sys, getopt, io, subprocess
 
 def update(outputfile):
-    url = "https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml"
+    url = "https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/" + outputfile
     
-    print("Retrieving latest revision of gl.xml")
+    print("Retrieving latest revision of " + outputfile)
 
     os.system("curl " + url + " > " + outputfile)
     os.system("git diff " + outputfile)
@@ -15,7 +15,7 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv[1:], "f:", ["file="])
     except getopt.GetoptError:
-        print("usage: %s -f <GL XML>" % argv[0])
+        print("usage: %s -f <XML>" % argv[0])
         sys.exit(1)
     
     outputfile = None
@@ -25,7 +25,7 @@ def main(argv):
             outputfile = arg
 
     if outputfile == None:
-        print("no GL XML file given")
+        print("no XML file given")
         sys.exit(1)
 
     update(outputfile)
