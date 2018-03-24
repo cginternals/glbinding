c70aaae4ff4c9fdf0a7cc4747f589ae4db777829
Willy Scheibel <willyscheibel@gmx.de>
2018-03-24 22:57:39 +0100

    Remove obsolete shell scripts (prefer python scripts instead)

--- source/codegeneration/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python scripts/generate.py -s gl.xml -d -a gl ../glbinding -r gl.revision -p patch.xml
-python scripts/generate.py -s gl.xml -a glsc -d ../glscbinding -r gl.revision -p patch.xml
--- source/codegeneration/update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python scripts/update.py -f gl.xml
