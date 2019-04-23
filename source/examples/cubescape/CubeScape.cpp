16d5d987bd65a6278066b3601b695b811fe943df
Willy Scheibel <willy.scheibel@hpi.de>
2019-04-23 17:03:35 +0200

    CMake setup review regarding dependencies and fallbacks

--- source/examples/cubescape/CubeScape.cpp
+++ source/examples/cubescape/CubeScape.cpp
@@ -6,7 +6,9 @@
 #include <fstream>
 #include <cmath>
 
+#ifdef cpplocate_FOUND
 #include <cpplocate/cpplocate.h>
+#endif
 
 #include <glbinding/gl/gl.h>
 
@@ -32,20 +34,15 @@ bool readFile(const std::string & filePath, std::string & content)
     return true;
 }
 
-// convenience
-std::string readFile(const std::string & filePath)
-{
-    std::string content;
-    readFile(filePath, content);
-
-    return content;
-}
-
 std::string determineDataPath()
 {
+#ifdef cpplocate_FOUND
     std::string path = cpplocate::locatePath("data/cubescape", "share/glbinding/cubescape", reinterpret_cast<void *>(&gl::glCreateShader));
     if (path.empty()) path = "./data";
     else              path = path + "/data";
+#else
+    const std::string path = "./data";
+#endif
 
     return path;
 }
@@ -55,7 +52,8 @@ std::string determineDataPath()
 
 
 CubeScape::CubeScape()
-: a_vertex(-1)
+: m_initialized(false)
+, a_vertex(-1)
 , u_transform(-1)
 , u_time(-1)
 , u_numcubes(-1)
@@ -73,9 +71,19 @@ CubeScape::CubeScape()
     GLuint gs = glCreateShader(GL_GEOMETRY_SHADER);
     GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
 
-    std::string vertexSource   = readFile(dataPath + "/cubescape/cubescape.vert");
-    std::string geometrySource = readFile(dataPath + "/cubescape/cubescape.geom");
-    std::string fragmentSource = readFile(dataPath + "/cubescape/cubescape.frag");
+    std::string vertexSource;
+    std::string geometrySource;
+    std::string fragmentSource;
+
+    auto success = readFile(dataPath + "/cubescape/cubescape.vert", vertexSource);
+    success &= readFile(dataPath + "/cubescape/cubescape.geom", geometrySource);
+    success &= readFile(dataPath + "/cubescape/cubescape.frag", fragmentSource);
+
+    if (!success)
+    {
+        std::cerr << "Could not load shaders in " << dataPath + "/cubescape/cubescape.*" << "." << std::endl;
+        return;
+    }
 
     const char * vertSource = vertexSource.c_str();
     const char * geomSource = geometrySource.c_str();
@@ -202,10 +210,17 @@ CubeScape::CubeScape()
     // view
 
     m_view = mat4::lookAt(0.f, 0.8f,-2.0f, 0.f, -1.2f, 0.f, 0.f, 1.f, 0.f);
+
+    m_initialized = true;
 }
 
 CubeScape::~CubeScape()
 {
+    if (!m_initialized)
+    {
+        return;
+    }
+
     glDeleteBuffers(1, &m_vertices);
     glDeleteBuffers(1, &m_indices);
 
@@ -230,6 +245,11 @@ void CubeScape::resize(int width, int height)
 
 void CubeScape::draw()
 {
+    if (!m_initialized)
+    {
+        return;
+    }
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(clock::now() - m_time);
