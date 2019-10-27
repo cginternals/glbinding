3af0f5dca78b90ac5850f73ab42ebedfeebdaae1
dlyr <github@dlyr.fr>
2019-10-27 10:54:23 +0100

    fail to compile boolean example

---
+++ source/examples/boolean/main.cpp
@@ -0,0 +1,10 @@
+#include <glbinding/gl45core/boolean.h>
+
+int main()
+{
+
+const gl45core::GLboolean b1 = gl45core::GL_TRUE;
+const gl::GLboolean b2 = gl::GL_TRUE;
+const GLboolean b3 = GL_TRUE;
+
+}
