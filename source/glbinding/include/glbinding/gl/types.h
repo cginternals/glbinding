75663469b465cca6654bbf79b698be1fc984ee8d
Willy Scheibel <willy.scheibel@hpi.de>
2024-02-27 11:34:06 +0100

    Renegerate using current template

--- source/glbinding/include/glbinding/gl/types.h
+++ source/glbinding/include/glbinding/gl/types.h
@@ -9,6 +9,7 @@
 
 #include <KHR/khrplatform.h>
 
+
 #ifdef _MSC_VER
 #define GL_APIENTRY __stdcall
 #else
@@ -25,36 +26,11 @@ namespace gl
 {
 
 
-struct _cl_context;
-struct _cl_event;
 #ifdef __APPLE__
 typedef void *GLhandleARB;
 #else
 typedef unsigned int GLhandleARB;
 #endif
-enum class AttribMask : unsigned int;
-enum class BufferStorageMask : unsigned int;
-enum class ClearBufferMask : unsigned int;
-enum class ClientAttribMask : unsigned int;
-enum class ContextFlagMask : unsigned int;
-enum class ContextProfileMask : unsigned int;
-enum class FfdMaskSGIX : unsigned int;
-enum class FragmentShaderColorModMaskATI : unsigned int;
-enum class FragmentShaderDestMaskATI : unsigned int;
-enum class FragmentShaderDestModMaskATI : unsigned int;
-enum class GLenum : unsigned int;
-enum class GLextension : int;
-enum class MapBufferAccessMask : unsigned int;
-enum class MemoryBarrierMask : unsigned int;
-enum class OcclusionQueryEventMaskAMD : unsigned int;
-enum class PathFontStyle : unsigned int;
-enum class PathMetricMask : unsigned int;
-enum class PerformanceQueryCapsMaskINTEL : unsigned int;
-enum class SyncObjectMask : unsigned int;
-enum class TextureStorageMaskAMD : unsigned int;
-enum class UnusedMask : unsigned int;
-enum class UseProgramStageMask : unsigned int;
-enum class VertexHintsMaskPGI : unsigned int;
 using GLbitfield = unsigned int;
 using GLbyte = khronos_int8_t;
 using GLchar = char;
@@ -85,14 +61,39 @@ using GLuint = unsigned int;
 using GLuint64 = khronos_uint64_t;
 using GLuint64EXT = khronos_uint64_t;
 using GLushort = khronos_uint16_t;
-using GLvdpauSurfaceNV = GLintptr;
 using GLvoid = void;
+using GLvdpauSurfaceNV = GLintptr;
 using VertexShaderWriteMaskEXT = GLboolean;
+enum class AttribMask : unsigned int;
+enum class BufferStorageMask : unsigned int;
+enum class ClearBufferMask : unsigned int;
+enum class ClientAttribMask : unsigned int;
+enum class ContextFlagMask : unsigned int;
+enum class ContextProfileMask : unsigned int;
+enum class FfdMaskSGIX : unsigned int;
+enum class FragmentShaderColorModMaskATI : unsigned int;
+enum class FragmentShaderDestMaskATI : unsigned int;
+enum class FragmentShaderDestModMaskATI : unsigned int;
+enum class GLenum : unsigned int;
+enum class GLextension : int;
+enum class MapBufferAccessMask : unsigned int;
+enum class MemoryBarrierMask : unsigned int;
+enum class OcclusionQueryEventMaskAMD : unsigned int;
+enum class PathFontStyle : unsigned int;
+enum class PathMetricMask : unsigned int;
+enum class PerformanceQueryCapsMaskINTEL : unsigned int;
+enum class SyncObjectMask : unsigned int;
+enum class TextureStorageMaskAMD : unsigned int;
+enum class UnusedMask : unsigned int;
+enum class UseProgramStageMask : unsigned int;
+enum class VertexHintsMaskPGI : unsigned int;
 using GLDEBUGPROC = void (GL_APIENTRY *)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
 using GLDEBUGPROCAMD = void (GL_APIENTRY *)(GLuint id,GLenum category,GLenum severity,GLsizei length,const GLchar *message,void *userParam);
 using GLDEBUGPROCARB = void (GL_APIENTRY *)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
 using GLDEBUGPROCKHR = void (GL_APIENTRY *)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
 using GLVULKANPROCNV = void (GL_APIENTRY *)(void);
+struct _cl_context;
+struct _cl_event;
 
 
 } // namespace gl
